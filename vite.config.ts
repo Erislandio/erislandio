2d4ae426d7796b426b76812a63127bf9189cdf29
Erislandio <erislandiosoares21@gmail.com>
2026-03-12 16:22:38 -0300

    feat: use single html build and fix package deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portifolio/',
-})
+  plugins: [react(), viteSingleFile()],
+  base: "/",
+});
